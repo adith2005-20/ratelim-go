1ad3fd520181cea384f9bef9961f274a1d145c7e
adith2005-20 <132918418+adith2005-20@users.noreply.github.com>
2025-11-06 13:16:46 +0530

    feat: moved to atomic int based limiting

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/adith2005-20/ratelim-go
 
 go 1.24.3
+
+require github.com/benbjohnson/clock v1.3.5 // indirect
--- internal/limiter/limiter.go
+++ internal/limiter/limiter.go
@@ -1,46 +1,155 @@
 package limiter
-
+// INT64 based atomic rate limiter
 import (
-	"sync"
+	"sync/atomic"
 	"time"
+	"github.com/benbjohnson/clock"
 )
 
-type Limiter struct {
-	rate	int 
-	window	time.Duration
-	mu		sync.Mutex
-	allowance int
-	lastCheck	time.Time
+type Limiter interface {
+	Take() time.Time
 }
 
-func New(rate int, window time.Duration) *Limiter {
-	return &Limiter{
-		rate:	rate,
-		window:	window,
-		allowance: rate,
-		lastCheck: time.Now(),
-	}
+type atomicInt64Limiter struct {
+	prepadding [64]byte
+	state int64
+	postpadding [56]byte
+
+	perRequest time.Duration
+	maxSlack time.Duration
+	clock clock.Clock
 }
 
-func (l *Limiter) Allow() bool {
-	l.mu.Lock()
-	defer l.mu.Unlock()
+type config struct {
+	clock clock.Clock 
+	slack int
+	per time.Duration
+}
 
+func New(rate int, opts ...Option) Limiter {
+	return newAtomicInt64Based(rate, opts...)
+}
 
-	now:= time.Now()
-	elapsed:= now.Sub(l.lastCheck)
-	l.lastCheck= now
-
-	l.allowance= int(float64(elapsed)/float64(l.window) * float64(l.rate))
-
-	if l.allowance > l.rate {
-		l.allowance = l.rate
+func buildConfig(opts []Option) config{
+	c := config{
+		clock: clock.New(),
+		slack: 10,
+		per:   time.Second,
 	}
 
-	if l.allowance< 1 {
-		return false
+	for _, opt := range opts {
+		opt.apply(&c)
+	}
+	return c
+}
+
+func newAtomicInt64Based(rate int, opts ...Option) *atomicInt64Limiter {
+	config:=buildConfig(opts)
+	perRequest:=config.per / time.Duration(rate)
+	l:=&atomicInt64Limiter{
+		perRequest: perRequest,
+		maxSlack: time.Duration(config.slack)*perRequest,
+		clock: config.clock,
+	}
+	atomic.StoreInt64(&l.state, 0)
+	return l
+}
+
+func (t *atomicInt64Limiter) Take() time.Time {
+	var (
+		newTimeOfNextPermissionIssue int64
+		now                          int64
+	)
+	for {
+		now = t.clock.Now().UnixNano()
+		timeOfNextPermissionIssue := atomic.LoadInt64(&t.state)
+
+		switch {
+		case timeOfNextPermissionIssue == 0 || (t.maxSlack == 0 && now-timeOfNextPermissionIssue > int64(t.perRequest)):
+			newTimeOfNextPermissionIssue = now
+		case t.maxSlack > 0 && now-timeOfNextPermissionIssue > int64(t.maxSlack)+int64(t.perRequest):
+			
+			newTimeOfNextPermissionIssue = now - int64(t.maxSlack)
+		default:
+			
+			newTimeOfNextPermissionIssue = timeOfNextPermissionIssue + int64(t.perRequest)
+		}
+
+		if atomic.CompareAndSwapInt64(&t.state, timeOfNextPermissionIssue, newTimeOfNextPermissionIssue) {
+			break
+		}
 	}
 
-	l.allowance--
-	return true
+	sleepDuration := time.Duration(newTimeOfNextPermissionIssue - now)
+	if sleepDuration > 0 {
+		t.clock.Sleep(sleepDuration)
+		return time.Unix(0, newTimeOfNextPermissionIssue)
+	}
+	return time.Unix(0, now)
+}
+
+
+///////////////
+
+
+type Option interface {
+	apply(*config)
+}
+
+type clockOption struct {
+	clock clock.Clock
+}
+
+func (o clockOption) apply(c *config) {
+	c.clock = o.clock
+}
+
+// WithClock returns an option for ratelimit.New that provides an alternate
+// Clock implementation, typically a mock Clock for testing.
+func WithClock(clock clock.Clock) Option {
+	return clockOption{clock: clock}
+}
+
+type slackOption int
+
+func (o slackOption) apply(c *config) {
+	c.slack = int(o)
+}
+
+// WithoutSlack configures the limiter to be strict and not to accumulate
+// previously "unspent" requests for future bursts of traffic.
+var WithoutSlack Option = slackOption(0)
+
+// WithSlack configures custom slack.
+// Slack allows the limiter to accumulate "unspent" requests
+// for future bursts of traffic.
+func WithSlack(slack int) Option {
+	return slackOption(slack)
+}
+
+type perOption time.Duration
+
+func (p perOption) apply(c *config) {
+	c.per = time.Duration(p)
+}
+
+// Per allows configuring limits for different time windows.
+//
+// The default window is one second, so New(100) produces a one hundred per
+// second (100 Hz) rate limiter.
+//
+// New(2, Per(60*time.Second)) creates a 2 per minute rate limiter.
+func Per(per time.Duration) Option {
+	return perOption(per)
+}
+
+type unlimited struct{}
+
+// NewUnlimited returns a RateLimiter that is not limited.
+func NewUnlimited() Limiter {
+	return unlimited{}
+}
+
+func (unlimited) Take() time.Time {
+	return time.Now()
 }
